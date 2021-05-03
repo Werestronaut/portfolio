917765030ab5e5f77ad0c859f8694667f7d7c5df
Werestronaut <72190556+Werestronaut@users.noreply.github.com>
2021-05-03 11:07:49 +0200

    Typography
    - Actually added the font to index.html
    - Edited font color and sizing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="home">
-  <h1>Home</h1>
-
     <v-row>
       <v-col cols="12" md="6" offset="1" class="lighten-5" id="front-text">
-        Text zone
-
         <h3 class="text-right">God eftermiddag!</h3>
-        <h4 class="text-right">Tag et kig rundt og se om du finder noget du kan lide</h4>
-        
+        <h4 class="text-right">Tag et kig rundt og se om du finder noget du kan lide</h4>      
         </v-col>
     </v-row>
 
@@ -23,17 +18,19 @@ h3 {
   font-family: halyard-display,sans-serif;
   font-weight: 600;
   font-style: normal;
+  color: #3A3A3A;
 }
 
 h4 {
   font-size: 390%;
   line-height: 110%;
-  font-family: halyard-display, sans-serif;
-  font-weight: 500;
-  font-style: normal;
+  font-family: halyard-display, sans-serif; 
+  font-weight: 300; 
+  font-style: normal; 
+  color: #3A3A3A;
 }
 
-</style>>
+</style>
 
 <script>
 
