aed59fa71cdbaa93f4aca85ebde2fd2e1ec98e67
Werestronaut <72190556+Werestronaut@users.noreply.github.com>
2021-05-03 10:58:59 +0200

    Frontpage edits
    - Removed col-box/image area.
    - Changed up typography
       - Added fonts (Halyard Display)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,37 @@
   <h1>Home</h1>
 
     <v-row>
-      <v-col cols="12" md="4" class="purple lighten-5">
+      <v-col cols="12" md="6" offset="1" class="lighten-5" id="front-text">
         Text zone
-        </v-col>
-      <v-col  cols="12" sm="6" md="8" class="blue lighten-5">
-        Billede zone
+
+        <h3 class="text-right">God eftermiddag!</h3>
+        <h4 class="text-right">Tag et kig rundt og se om du finder noget du kan lide.</h4>
+        
         </v-col>
     </v-row>
 
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+h3 {
+  font-size: 100px;
+  line-height: 110%;
+  font-family: halyard-display,sans-serif;
+  font-weight: 600;
+  font-style: normal;
+}
+
+h4 {
+  font-size: 60px;
+  line-height: 110%;
+  font-family: halyard-display, sans-serif;
+  font-weight: 500;
+  font-style: normal;
+}
+
+</style>>
 
 <script>
 
